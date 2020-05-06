8a6a26fe2fafc8bd4f1feb1740a20c50d10dbb8a
Rafael Pastrana <pastrana@arch.ethz.ch>
2020-05-05 22:21:26 -0500

    Created loss plotter for multiple models

--- scripts/kmeans_errors_field_angles.py
+++ scripts/kmeans_errors_field_angles.py
@@ -38,122 +38,6 @@ from variational_clustering.clustering import k_means
 from variational_clustering.helpers import align_vector
 
 
-def vector_lines_on_faces(mesh, vector_tag, uniform=True, factor=0.02):
-	'''
-	'''
-	def line_sdl(start, direction, length):
-		direction = normalize_vector(direction[:])
-		a = add_vectors(start, scale_vector(direction, -length))
-		b = add_vectors(start, scale_vector(direction, +length))
-
-		return a, b
-
-	lines = []
-	for fkey, attr in mesh.faces(data=True):
-		vector = attr.get(vector_tag)
-
-		if not vector:
-			raise ValueError('Vector {} not defined on face {}'.format(vector_tag, fkey))
-
-		if uniform:
-			vec_length = factor
-		else:
-			vec_length = length_vector(vector) * factor
-
-		pt = mesh.face_centroid(fkey)
-		lines.append(line_sdl(pt, vector, vec_length))
-	return lines
-
-
-def line_tuple_to_dict(line):
-	'''
-	'''
-	a, b = line
-	return {'start': a, 'end': b}
-
-
-def polygon_list_to_dict(polygon):
-	return {'points': polygon}
-
-
-def extract_polygons_from_contours(contours, levels):
-	polygons = []
-	for i in range(len(contours)):
-	    level = levels[i]
-	    contour = contours[i]
-
-	    for path in contour:
-	        for polygon in path:
-	            polygons.append(polygon[:-1])
-	return polygons
-
-
-def scalarfield_contours_numpy(xy, s, levels=50, density=100, method='cubic'):
-
-    xy = asarray(xy)
-    s = asarray(s)
-    x = xy[:, 0]
-    y = xy[:, 1]
-
-    X, Y = meshgrid(linspace(amin(x), amax(x), 2 * density),
-                    linspace(amin(y), amax(y), 2 * density))
-
-    S = griddata((x, y), s, (X, Y), method=method)
-
-    fig = plt.figure()
-    ax = fig.add_subplot(111, aspect='equal')
-    c = ax.contour(X, Y, S, levels)
-
-    contours = [0] * len(c.collections)
-    levels = c.levels
-
-    for i, coll in enumerate(iter(c.collections)):
-        paths = coll.get_paths()
-        contours[i] = [0] * len(paths)
-        for j, path in enumerate(iter(paths)):
-            polygons = path.to_polygons()
-            contours[i][j] = [0] * len(polygons)
-            for k, polygon in enumerate(iter(polygons)):
-                contours[i][j][k] = polygon
-
-    plt.close(fig)
-
-    return levels, contours
-
-
-def contour_polygons(mesh, centers, face_labels, density=100, method='nearest'):
-	xy, s = [], []
-
-	for fkey in mesh.faces():
-		point = mesh.face_centroid(fkey)[:2]
-		xy.append(point)
-		a = face_labels[fkey][0]
-		s.append(a)
-
-	b = np.sort(centers, axis=0).flatten()
-
-	levels, contours = scalarfield_contours_numpy(xy, s, levels=b, density=density, method=method)
-	polygons = extract_polygons_from_contours(contours, levels)
-
-	polygons = [p for p in map(polygon_list_to_dict, polygons)]
-	for p in polygons:
-		p['edgewidth'] = 1.0
-
-	return polygons
-
-
-def rgb_colors(data):
-	assert isinstance(data, dict)
-
-	valuemax = np.amax(np.array(list(data.values())))
-
-	colors = {}
-	for idx, value in data.items():
-		colors[idx] = i_to_rgb(value / valuemax)  # tuple 0-255
-
-	return colors
-
-
 def cluster(data, n_clusters, reshape=None, normalize=False, random_state=0, n_jobs=-1):
 	assert isinstance(data, dict)
 
@@ -341,7 +225,7 @@ if __name__ == '__main__':
 			errors.append(k_error)
 
 		
-		plt.plot(errors, marker='o', label=filename)
+		plt.plot(errors, marker='o', label=filepath)
 	
 	# ==========================================================================
 	# Show
